ad0ad57a47b5d5ed8ba6793332bd08e92b0f37fa
ZE Personal <zepersonal@Zains-MacBook-Pro.local>
2025-04-22 21:18:26 +0400

    fix: update vite base since the website will be on the root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
